a63c6d12e0454dd7aac811f2a850d46097942463
Sayak Chatterjee <samarpita1947@gmail.com>
2025-10-08 20:14:28 +0530

    added for loop

--- src/loops/Test.java
+++ src/loops/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
 
         //While loop
-    }
+
 //        int i = 0;
 //        while(i<=5){
 //            System.out.println(i);
@@ -19,10 +19,16 @@ public class Test {
 //            i++;
 //        }while (i<=5);
 //    }
-      //
+      // for loop
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
 
 
 
 
+
+
+    }
 
 }
